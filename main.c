28611976469a34828b315f455ac3349bd404d294
NatchimieDylan <natchimiedylan@gmail.com>
2019-05-30 00:58:10 +0200

    Creation d'une fenetre et d'un rendu

--- main.c
+++ main.c
@@ -2,23 +2,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void ExitWithError(char *message){
+#include "function.h"
+
+void ExitWithError(char *message)
+{
 	SDL_Log("ERREUR %s -> %s", message,SDL_GetError());
 	SDL_Quit();
 	exit(EXIT_FAILURE);
 }
 
-
-
-
-int main(int argc, char *argv[])
+void Initialise(SDL_Window *fenetre, SDL_Renderer *rendu)
 {
+	/*-----------Initilisation de la SDL-----------*/
+
 	if (SDL_Init(SDL_INIT_EVERYTHING)!=0){
-		SDL_Log("ERREUR %s")
+		ExitWithError("Impossible de charger la SDL");
+	}
+
+	/*-----------FENETRE-----------*/
+
+	fenetre=SDL_Window("Jeu du Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,900,700,0);
+
+	if(fenetre==NULL){
+		ExitWithError("Impossible de créer une fenetre");
+	} 
+
+	/*-----------RENDU-----------*/
+
+	rendu=SDL_CreateRenderer(fenetre,-1,SDL_RENDERER_ACCELERATED);
+
+	if (rendu==NULL){
+		SDL_DestroyWindow(fenetre);
+		ExitWithError("Impossible de créer un rendu");
 
 	}
 
+}
 
+
+int main(int argc, char *argv[])
+{	
+	/*--------Initialisation de la fenêtre et du rendu--------*/
+
+	SDL_Window *fenetre=NULL;
+	SDL_Renderer *rendu=NULL; 
+
+	Initialise(fenetre,rendu);
+
+
+	/*Execution du programme*/
+
+	SDL_DestroyWindow(fenetre);
+	SDL_DestroyRenderer(rendu);
 	SDL_Quit();
 
 	return 0;
