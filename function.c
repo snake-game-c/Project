728f2b9b953bfcecd14565ee7840c57bac1919ed
NatchimieDylan <natchimiedylan@gmail.com>
2019-05-29 23:38:57 +0200

    Modification de la structure SnakeCell, ajout de direction et modificiation de la fonction AddRectangle

--- function.c
+++ function.c
@@ -4,24 +4,54 @@
 
 #include "function.h"
 
-SnakeCell *AddRectangle(Snake *snake, SDL_Rect rec)
+SnakeCell *AddRectangle(Snake *snake)
 {
 	/*Initialisation de la nouvelle cellule*/
+
+	SDL_Rect *rec=malloc(sizeof(SDL_Rect));
+	rec->w=COTE;
+	rec->h=COTE;
 
 	SnakeCell *newrect=malloc(sizeof(SnakeCell));
 	newrect->rectangle=rec;
 	newrect->next=NULL;
 
-	/*Ajout du rectangle*/
+	/*------------------------------------*/
 
-	if(Snake->length<3){
-		if 
-	}
+	/*--------Ajout du rectangle----------*/
 
-	SnakeCell *current=snake;
+	SnakeCell *current=snake->head; 
+
+	//On atteint le pointeur de la dernière cellule
 	while (current->next!=NULL){
 		current=current->next;
 	}
+
+	//On part du point d'origine du dernier rectangle
+	rec->x=current->rectangle->x;
+	rec->y=current->rectangle->y;
+
+	/*On adapte le point de rec suivant la direction de la dernière cellule*/
+
+	if (current->direction==NORTH){
+	    rec->x+=COTE;
+	}
+	else if (current->direction==EAST){
+		rec->y-=COTE;
+	}
+	else if (current->direction==WEST){
+		rec->y+=COTE;
+	}
+	else if (current->direction==SOUTH){
+		rec->x-=COTE;
+	}
+
+	//On oublie pas de donner la même direction au rectangle qu'on vient d'ajouter
+
+	newrect->direction=current->direction;
 	current->next=newrect;
-	newrect=
+
+	/*------------------------------------*/
+
+	return snake;
 }
--- function.h
+++ function.h
@@ -1,12 +1,31 @@
 #ifndef __FUNCTION__H__
 #define __FUNCTION__H__
 
+//On définit quatre constantes qui vont nous permettre de connaitre la direction dans laquelle avance chaque rectangle du Snake a chaque instant.
+#define NORTH 0
+#define EAST 1
+#define SOUTH 2
+#define WEST 3
+
+#define COTE 15 //taille des rectangles composant le snake en pixel
+
+
+//Le Snake est réprésenté comme une liste chainée
+
+/*Une cellule du Snake(contient un rectangle)*/
 typedef struct SnakeCell{
-	SDL_Rect rectangle;
-	SDL_Rect *next;
+	SDL_Rect rectangle;  //chaque cellule contient un rectangle
+	SDL_Rect *next; //un pointeur vers la prochaine cellule
+	int direction;  //la direction courante du rectangle
 }SnakeCell;
 
-SnakeCell *AddRectangle(SnakeCell *snake, SDL_Rect rec);
+/*Structure Snake*/
+typedef struct Snake{
+	SnakeCell *head; //Contient la tête du Snake
+	int length; //la longueur du Snake
+}Snake;
+
+SnakeCell *AddRectangle(SnakeCell *snake);
 void PrintSnake(SnakeCell *snake, SDL_Window *window, SDL_Window *window, SDL_Renderer *renderer);
 
 
